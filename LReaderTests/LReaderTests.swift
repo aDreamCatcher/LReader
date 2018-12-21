e89f6f63ab346b7371f066dfec7238e6ce22e7c3
aDreamCatcher <344944410@qq.com>
2018-12-21 18:53:52 +0800

    Initial Commit

---
+++ LReaderTests/LReaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  LReaderTests.swift
+//  LReaderTests
+//
+//  Created by lgy on 2018/12/21.
+//  Copyright © 2018 lgy. All rights reserved.
+//
+
+import XCTest
+@testable import LReader
+
+class LReaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
